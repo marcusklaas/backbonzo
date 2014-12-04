1fbe6bea5e4eaa08cb803bddcb7dc2598e382959
Marcus Klaas <mail@marcusklaas.nl>
2014-12-04 13:08:06 +0100

    split database and crypto functions to their own modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,28 @@
 #![feature(slicing_syntax)]
 
 extern crate rusqlite;
-extern crate "rust-crypto" as crypto;
-extern crate time;
 
-use rusqlite::SqliteConnection;
 use std::io::IoResult;
 use std::io::fs::{readdir, walk_dir, File, PathExtensions};
 use std::path::posix::Path;
+use rusqlite::{SqliteConnection, SqliteError};
 
-use crypto::{aes, buffer, symmetriccipher};
-use crypto::digest::Digest;
-use crypto::buffer::{WriteBuffer, ReadBuffer};
-use crypto::blockmodes::PkcsPadding;
+mod database;
+mod crypto;
 
 static DATABASE_FILE: &'static str = "index.db3";
-static BLOCK_SIZE: uint = 1024 * 1024;
-static TEST_KEY: &'static str = "testkey123";
 static TEMP_INPUT_DIRECTORY: &'static str = ".";
 static TEMP_OUTPUT_DIRECTORY: &'static str = "/tmp/";
+static BLOCK_SIZE: uint = 1024 * 1024;
 
-/* TODO: there should be a type Result<T, String> because we use this all the time. using String instead of &'static str allows to return dynamic error messages */
+/* TODO: there should be a different type Result<T, SomeEnum> because we use this all the time. using String instead of &'static str allows to return dynamic error messages */
+
+enum BonzoError {
+    DatabaseError(SqliteError),
+    IoError
+}
+
+type BonzoResult<T> = Result<T, BonzoError>;
 
 struct Blocks<'a> {
     file: File,
@@ -66,7 +68,9 @@ enum Directory {
 pub fn init() -> Result<(), &'static str> {
     println!("Setting up database..");
     
-    let connection = match setup_database() {
+    let database_path = Path::new(DATABASE_FILE);
+    
+    let connection = match database::setup(&Path::new(DATABASE_FILE)) {
         Ok(conn) => conn,
         Err(e)   => return Err(e)
     };
@@ -110,7 +114,7 @@ fn export_directory(connection: &SqliteConnection, path: &Path, directory: Direc
             Some(s) => s
         };
         
-        let child_directory = try!(get_directory_id(connection, directory, name));
+        let child_directory = try!(database::get_directory_id(connection, directory, name));
     
         try!(export_directory(connection, directory_path, child_directory));
     }
@@ -118,42 +122,8 @@ fn export_directory(connection: &SqliteConnection, path: &Path, directory: Direc
     Ok(())
 }
 
-fn hash_file(path: &Path) -> Result<String, &'static str> {
-    let mut hasher = crypto::sha2::Sha256::new();
-    
-    let mut blocks = match Blocks::from_path(path, 1024) {
-        Some(blocks) => blocks,
-        None         => return Err("Couldn't read file")
-    };
-    
-    loop {
-        match blocks.next() {
-            Some(slice) => hasher.input(slice),
-            None        => break
-        }
-    }
-    
-    Ok(hasher.result_str())
-}
-
-fn file_known(connection: &SqliteConnection, path: &Path) -> Result<(bool, String), &'static str> {
-    let hash: String = match hash_file(path) {
-        Err(e)     => return Err(e),
-        Ok(string) => string
-    };
-
-    let known = connection.query_row(
-        "SELECT COUNT(id) FROM file
-        WHERE hash = $1;",
-        &[&hash.as_slice()],
-        |row| row.get::<i64>(0) > 0
-    );
-    
-    Ok((known, hash))
-}
-
 fn export_file(connection: &SqliteConnection, directory: Directory, path: &Path) -> Result<(), &'static str> {
-    let hash = match file_known(connection, path) {
+    let hash = match database::file_known(connection, path) {
         Ok((true, _))     => return Ok(()),
         Err(e)            => return Err(e),
         Ok((false, hash)) => hash
@@ -178,109 +148,21 @@ fn export_file(connection: &SqliteConnection, directory: Directory, path: &Path)
         }
     }
     
-    if !index_persist_file(connection, directory, path, hash.as_slice(), block_id_list.as_slice()) {
+    if !database::persist_file(connection, directory, path, hash.as_slice(), block_id_list.as_slice()) {
         println!("Failed persisting file to database");
     }
     
     Ok(())
 }
 
-// FIXME: should return Result<(), &'static str> so we can more info what potentially went wrong
-fn index_persist_file(connection: &SqliteConnection, directory: Directory, path: &Path, hash: &str, block_id_list: &[uint]) -> bool {
-    let filename_bytes = match path.filename() {
-        None        => return false,
-        Some(bytes) => bytes
-    };
-
-    let filename: String = String::from_utf8_lossy(filename_bytes).into_owned();
-
-    let transaction = match connection.transaction() {
-        Ok(tx) => tx,
-        Err(_) => return false
-    };
-
-    match connection.execute("INSERT INTO file (hash) VALUES ($1);", &[&hash]) {
-        Err(_) => return false,
-        Ok(..) => ()
-    }
-    
-    let file_id = connection.last_insert_rowid();
-    
-    for (ordinal, block_id) in block_id_list.iter().enumerate() {
-        let result = connection.execute(
-            "INSERT INTO fileblock (file_id, block_id, ordinal) VALUES ($1, $2, $3);"
-            , &[&(file_id as i64), &(*block_id as i64), &(ordinal as i64)]
-        );
-    
-        match result {
-            Err(_) => return false,
-            Ok(..) => ()
-        }
-    }
-    
-    let alias_query = "INSERT INTO alias (directory_id, file_id, name, timestamp) VALUES ($1, $2, $3, $4);";
-    let timestamp = time::get_time().sec;
-    let directory_id: Option<i64> = match directory {
-        Directory::Root      => None,
-        Directory::Child(id) => Some(id as i64)
-    };
-    
-    match connection.execute(alias_query, &[&directory_id, &(file_id as i64), &filename, &(timestamp as i64)]) {
-        Err(e) => return false,
-        Ok(..) => ()
-    }
-
-    transaction.commit().is_ok()
-}
-
-fn get_directory_id(connection: &SqliteConnection, parent: Directory, name: &str) -> Result<Directory, &'static str> {
-    let parent_id: Option<i64> = match parent {
-        Directory::Root      => None,
-        Directory::Child(id) => Some(id as i64)
-    };
-
-    // TODO: escape name!
-    let select_query = "SELECT SUM(id) FROM directory WHERE name = $1 AND parent_id = $2;"; 
-    let directory_id: Option<i64> = connection.query_row(select_query, &[&name, &parent_id], |row| row.get(0));
-    
-    match directory_id {
-        Some(id) => return Ok(Directory::Child(id as uint)),
-        None     => ()
-    }
-    
-    let insert_query = "INSERT INTO directory (parent_id, name) VALUES ($1, $2);";
-    
-    match connection.execute(insert_query, &[&parent_id, &name]) {
-        Ok(..) => Ok(Directory::Child(connection.last_insert_rowid() as uint)),
-        Err(_) => Err("Failed persisting new directory to database")
-    }
-}
-
-fn hash_block(block: &[u8]) -> String {
-    let mut hasher = crypto::sha2::Sha256::new();
-    
-    hasher.input(block);
-    
-    hasher.result_str()
-}
-
-fn block_known(connection: &SqliteConnection, hash: &str) -> bool {
-    connection.query_row(
-        "SELECT COUNT(id) FROM block
-        WHERE hash = $1;",
-        &[&hash],
-        |row| row.get::<i64>(0) > 0
-    )
-}
-
 fn export_block(connection: &SqliteConnection, block: &[u8]) -> Result<BlockExportResult, &'static str> {
-    let hash = hash_block(block);
+    let hash = crypto::hash_block(block);
 
-    if block_known(connection, hash.as_slice()) {
+    if database::block_known(connection, hash.as_slice()) {
         return Ok(BlockExportResult::Known);
     }
     
-    let bytes = match encrypt_block(block) {
+    let bytes = match crypto::encrypt_block(block) {
         Some(vector) => vector,
         None         => return Err("Failed encrypting block")
     };
@@ -290,7 +172,7 @@ fn export_block(connection: &SqliteConnection, block: &[u8]) -> Result<BlockExpo
         Ok(..) => ()
     }
     
-    match index_persist_block(connection, hash.as_slice()) {
+    match database::persist_block(connection, hash.as_slice()) {
         None     => Err("Failed persisting block to database"),
         Some(id) => Ok(BlockExportResult::New(id))
     }
@@ -308,140 +190,5 @@ fn write_block(hash: &str, bytes: &[u8]) -> IoResult<()> {
     match file.write(bytes) {
         Ok(..) => file.fsync(),
         Err(e) => Err(e)
-    }
-}
-
-fn index_persist_block(connection: &SqliteConnection, hash: &str) -> Option<uint> {
-    let result = connection.execute("INSERT INTO block (hash) VALUES ($1);", &[&hash]);
-    
-    match result {
-        Err(_) => None,
-        Ok(..) => Some(connection.last_insert_rowid() as uint)
-    }
-}
-
-fn encrypt_block(block: &[u8]) -> Option<Vec<u8>> {
-    let mut encryptor: Box<symmetriccipher::Encryptor> = aes::cbc_encryptor(
-        aes::KeySize::KeySize256,
-        TEST_KEY.as_bytes(),
-        &[],
-        PkcsPadding
-    );
-    
-    let mut final_result = Vec::<u8>::new();
-    let mut buffer = [0, ..4096];
-    let mut read_buffer = buffer::RefReadBuffer::new(block);
-    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
-    
-    while !read_buffer.is_empty() {
-        match encryptor.encrypt(&mut read_buffer, &mut write_buffer, true) {
-            Err(_) => return None,
-            Ok(..) => ()
-        }
-        
-        final_result.push_all(write_buffer.take_read_buffer().take_remaining());
-    }
-    
-    Some(final_result)
-}
-
-fn setup_database() -> Result<SqliteConnection, &'static str> {
-    let connection = try!(create_database());
-
-    if ! create_directory_table(&connection) {
-        return Err("Couldn't create directory table")
-    }
-    
-    if ! create_file_table(&connection) {
-        return Err("Couldn't create file table")
-    }
-    
-    if ! create_file_alias_table(&connection) {
-        return Err("Couldn't create file alias table")
-    }
-    
-    if ! create_block_table(&connection) {
-        return Err("Couldn't create block table")
-    }
-    
-    if ! create_file_block_table(&connection) {
-        return Err("Couldn't create file<->block table")
-    }
-    
-    Ok(connection)
-}
-
-fn create_directory_table(connection: &SqliteConnection) -> bool {
-    connection
-        .execute("CREATE TABLE directory (
-            id        INTEGER PRIMARY KEY,
-            parent_id INTEGER,
-            name      TEXT NOT NULL,
-            FOREIGN KEY(parent_id) REFERENCES directory(id)
-        );", &[])
-        .is_ok()
-}
-
-fn create_file_table(connection: &SqliteConnection) -> bool {
-    connection
-        .execute("CREATE TABLE file (
-            id           INTEGER PRIMARY KEY,
-            hash         TEXT NOT NULL
-        );", &[])
-        .is_ok()
-}
-
-fn create_file_alias_table(connection: &SqliteConnection) -> bool {
-    connection
-        .execute("CREATE TABLE alias (
-            id           INTEGER PRIMARY KEY,
-            directory_id INTEGER,
-            file_id      INTEGER,
-            name         TEXT NOT NULL,
-            timestamp    INTEGER,
-            FOREIGN KEY(directory_id) REFERENCES directory(id),
-            FOREIGN KEY(file_id) REFERENCES file(id)
-        );", &[])
-        .is_ok()
-}
-
-fn create_block_table(connection: &SqliteConnection) -> bool {
-    connection
-        .execute("CREATE TABLE block (
-            id           INTEGER PRIMARY KEY,
-            hash         TEXT NOT NULL
-        );", &[])
-        .is_ok()
-}
-
-fn create_file_block_table(connection: &SqliteConnection) -> bool {
-    connection
-        .execute("CREATE TABLE fileblock (
-            id           INTEGER PRIMARY KEY,
-            file_id      INTEGER NOT NULL,
-            ordinal      INTEGER NOT NULL,
-            block_id     INTEGER NOT NULL,
-            FOREIGN KEY(file_id) REFERENCES file(id),
-            FOREIGN KEY(block_id) REFERENCES block(id)
-        );", &[])
-        .is_ok()
-}
-
-fn create_database() -> Result<SqliteConnection, &'static str> {
-    let working_dir = Path::new(".");
-    let database_path = Path::new(DATABASE_FILE);
-    
-    let file_list = match readdir(&working_dir) {
-        Ok(list) => list,
-        Err(_)   => return Err("Failed reading directory")
-    };
-    
-    if file_list.into_iter().find(|path| *path == database_path).is_some() {
-        return Err("Database already exists");
-    }
-    
-    match SqliteConnection::open(DATABASE_FILE) {
-        Ok(conn) => Ok(conn),
-        Err(_)   => Err("Couldn't create database")
     }
 }
