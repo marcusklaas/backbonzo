22001fea3f41ae6f156253e22f1b093edda4fb8f
Marcus Klaas <mail@marcusklaas.nl>
2015-04-20 16:20:00 +0200

    read password from command-line

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
 #![cfg(not(test))]
+#![feature(libc)]
 
 extern crate rustc_serialize;
 extern crate backbonzo;
 extern crate docopt;
 extern crate time;
+extern crate termios;
+extern crate libc;
 
 use docopt::Docopt;
 use std::path::PathBuf;
+use std::error::Error;
 use time::Duration;
 use std::fmt::Debug;
-use std::io::{Write, stderr};
+use std::io::{Write, stderr, stdout, stdin};
 use backbonzo::{init, backup, restore, epoch_milliseconds, BonzoResult, AesEncrypter};
 
 static USAGE: &'static str = "
 backbonzo
 
 Usage:
-  backbonzo init    -k <key> -d <dest> [options]
-  backbonzo backup  -k <key>           [options]
-  backbonzo restore -k <key> -d <dest> [options]
+  backbonzo init    -d <dest> [options]
+  backbonzo backup            [options]
+  backbonzo restore -d <dest> [options]
   backbonzo --help
-  
+
 Options:
   -s --source=<source>       Source directory [default: ./].
   -d --destination=<dest>    Backup directory.
-  -k --key=<key>             Encryption key.
   -b --blocksize=<bs>        Size of blocks in kilobytes [default: 1000].
   -t --timestamp=<mseconds>  State to restore to in milliseconds since epoch [default: 0].
   -T --timeout=<seconds>     Maximum execution time in seconds [default: 0].
   -f --filter=<exp>          Glob expression for paths to restore [default: **].
-  -a --age=<days>            Number of days to retain old data [default: 183]. 
+  -a --age=<days>            Number of days to retain old data [default: 183].
 ";
 
 #[derive(RustcDecodable, Debug)]
@@ -40,20 +43,44 @@ struct Args {
     pub flag_destination: String,
     pub flag_source: String,
     pub flag_blocksize: u32,
-    pub flag_key: String,
     pub flag_timestamp: u64,
     pub flag_timeout: u64,
     pub flag_filter: String,
     pub flag_age: u32
 }
 
+fn fetch_password() -> String {
+    let optional_term = termios::Termios::from_fd(0).ok();
+
+    if let Some(mut term) = optional_term {
+        term.c_lflag &= !termios::ECHO;
+        term.c_lflag |= termios::ECHONL;
+
+        termios::tcsetattr(0, termios::TCSANOW, &term).unwrap();
+
+        print!("Passphrase: ");
+        stdout().flush().unwrap();
+    }
+
+    let mut password = String::new();
+    stdin().read_line(&mut password).unwrap();
+
+    if let Some(term) = optional_term {
+        termios::tcsetattr(0, termios::TCSANOW, &term).unwrap();
+    }
+
+    password.pop().unwrap();
+
+    password
+}
+
 fn main() {
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
+    let password = fetch_password();
     let source_path = PathBuf::from(&args.flag_source);
     let backup_path = PathBuf::from(&args.flag_destination);
-    let password = args.flag_key;
     let crypto_scheme = AesEncrypter::new(&password);
 
     if args.cmd_init {
